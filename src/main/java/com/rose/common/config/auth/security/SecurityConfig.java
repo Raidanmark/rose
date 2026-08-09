6b47b7d94be393a1f6fbd7506d8bb9d0f0dd9712
Daniil-Raider <Onidanil@Gmail.com>
2026-08-10 00:27:30 +0200

    Cors setuping (#15)
    * CI/CD setup
    
    * CI/CD setup
    
    * CI/CD setup
    
    * Created UserProfile endpoints
    
    * Created donations, added stripe connection, user payment account
    
    * Created donation controller,
    Websocket created
    
    * Created UserProfile endpoints
    
    * Revert "Created UserProfile endpoints"
    
    This reverts commit a5b29ee792d48c80ce4b8d855961660ddcbd68db.
    
    * Added frontend origin (#12)
    
    * Refresh token comes to cookie (#13)
    
    * Cors added at security config

--- src/main/java/com/rose/common/config/auth/security/SecurityConfig.java
+++ src/main/java/com/rose/common/config/auth/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,6 +20,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
