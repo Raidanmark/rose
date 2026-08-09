cbadc59b4036c1965f71fb46ed7694748fe16dc2
Raidanmark <Onidanil@Gmail.com>
2026-08-09 12:40:29 +0200

    Added frontend origin

--- src/main/java/com/rose/common/config/auth/security/SecurityConfig.java
+++ src/main/java/com/rose/common/config/auth/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.rose.common.config.auth.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,13 +26,15 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/auth/register"
                         ).permitAll()
 
                         .requestMatchers("/auth/register").permitAll()
 
                         .requestMatchers("/auth/login").permitAll()
                         .requestMatchers("/auth/refresh").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(
