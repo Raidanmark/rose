e4a3f470b38da42926304fa4b5a28c58c7ec3cb0
Raidanmark <Onidanil@Gmail.com>
2026-08-10 16:00:48 +0200

    Added support for specific tags

--- src/main/java/com/rose/common/config/auth/security/CorsConfig.java
+++ src/main/java/com/rose/common/config/auth/security/CorsConfig.java
@@ -27,7 +27,13 @@ public class CorsConfig {
                 "DELETE",
                 "OPTIONS"
         ));
-        config.setAllowedHeaders(List.of("*"));
+
+        config.setAllowedHeaders(List.of(
+                "Content-Type",
+                "Authorization"
+        ));
+
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
